753e4d70bc09303a3cf471d2d40a9ba5d87d3b5f
TRebirthC <57615170+TRebirthC@users.noreply.github.com>
2020-11-03 15:03:07 +0800

    Update matrixProduct.cpp

--- Project1/matrixProduct.cpp
+++ Project1/matrixProduct.cpp
@@ -39,10 +39,10 @@ Matrix matrixProduct(Matrix a, Matrix b)
 			for (int z = 0; z < ca; z++)
 			{
 				sum = sum + da[x + z] * db[y + z * cb];
-				cout << da[x + z] <<" ";
-				cout << db[y + z* cb] << endl;
+				//cout << da[x + z] <<" ";
+				//cout << db[y + z* cb] << endl;
 			}
-			cout << endl;
+			//cout << endl;
 			data[j * ra + i] = sum;
 		}
 	}
@@ -52,7 +52,7 @@ Matrix matrixProduct(Matrix a, Matrix b)
 
 int main()
 {
-	Matrix* a = new Matrix;
+	/*Matrix* a = new Matrix;
 	a->column = 3;
 	a->row = 2;
 	a->data = new float[6];
@@ -67,6 +67,23 @@ int main()
 	for (int i = 0; i < 6; i++)
 	{
 		b->data[i] = 6-i;
+	}*/
+
+	Matrix* a = new Matrix;
+	a->column = 20000;
+	a->row = 5000;
+	a->data = new float[100000000];
+	for (int i = 0; i < 100000000; i++)
+	{
+		a->data[i] = 1;
+	}
+	Matrix* b = new Matrix;
+	b->column = 5000;
+	b->row = 20000;
+	b->data = new float[100000000];
+	for (int i = 0; i < 100000000; i++)
+	{
+		b->data[i] = 1;
 	}
 
 	auto start = chrono::steady_clock::now();
@@ -80,10 +97,10 @@ int main()
 		<< chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms ≈ "
 		<< chrono::duration_cast<chrono::seconds>(end - start).count() << "s.\n";
 
-	for (int i = 0; i < 4; i++)
+	/*for (int i = 0; i < 4; i++)
 	{
 		cout << c.data[i] << " ";
-	}
+	}*/
 
 	delete a;
 	delete b;
