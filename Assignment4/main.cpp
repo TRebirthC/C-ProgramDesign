7ad570fda735202b4c6cd06ae2be8a7b966e0c1c
TRebirthC <57615170+TRebirthC@users.noreply.github.com>
2020-11-29 14:32:38 +0800

    Update main.cpp

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -11,18 +11,47 @@ int main()
 	da[3] = 2;
 	da[4] = 0;
 	Matrix a(2, 2, da);
+	cout << "Matrix a(2, 2, da)" << endl;
 	cout << a;
 	Matrix b(a);
-	cout << a;
+	cout << "Matrix b(a)" << endl;
 	cout << b;
 	Matrix c = b;
+	cout << "Matrix c = b" << endl;
 	cout << c;
 	Matrix d = 3 * c;
+	cout << "Matrix d = 3 * c" << endl;
 	cout << d;
 	Matrix e = d * 6;
+	cout << "Matrix e = d * 6" << endl;
 	cout << e;
-	a = b * c;
+	float* df = new float[2 * 3 + 1];
+	for (int i = 0; i < 6; i++)
+	{
+		df[i] = i;
+	}
+	df[6] = 0;
+	Matrix f(2, 3, df);
+	cout << "Matrix f(2, 3, df)" << endl;
+	cout << f;
+	float* dg = new float[2 * 3 + 1];
+	for (int i = 0; i < 6; i++)
+	{
+		dg[i] = 6-i;
+	}
+	dg[6] = 0;
+	Matrix g(3, 2, dg);
+	cout << "Matrix f(2, 3, dg)" << endl;
+	cout << g;
+	a = f * g;
+	cout << "a = f * g" << endl;
 	cout << a;
+	b = g * f;
+	cout << "b = g * f" << endl;
+	cout << b;
+	c = d * g;
+	cout << "c = d * g" << endl;
+	cout << c;
 	
 	return 0;
 }
